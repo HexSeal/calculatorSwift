0df5fb05cd725ba6fb088736878c336881250f5a
HexSeal <maxfin13@gmail.com>
2020-11-11 18:21:47 -0500

    Initial Commit

---
+++ BetterCalculator/BetterCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterCalculatorApp.swift
+//  BetterCalculator
+//
+//  Created by Maxwell Finn on 11/11/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
